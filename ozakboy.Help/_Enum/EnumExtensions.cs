41d3623e8cc261762426ec9cdc8958f1f24be49b
ozakboy <ozakboy@gmail.com>
2023-04-29 13:42:19 +0800

    新增 enum 從枚舉常數中獲取描述文本方法

---
+++ ozakboy.Help/_Enum/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+
+namespace ozakboy.Help
+{
+    public static  class EnumExtensions
+    {
+        /// <summary>
+        /// 從枚舉常數中獲取描述文本
+        /// </summary>
+        /// <param name="value">要獲取描述文本的枚舉常數</param>
+        /// <returns>枚舉常數的描述文本</returns>
+        public static string GetDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var descriptionAttribute =
+              (DescriptionAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(DescriptionAttribute), false);
+
+
+            return descriptionAttribute?.Description ?? value.ToString();
+        }
+    }
+}
