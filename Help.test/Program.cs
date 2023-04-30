35dce8f2114fe886146b73f86d13bbfd0e962d5d
ozakboy <ozakboy@gmail.com>
2023-04-30 13:01:03 +0800

    更新

---
+++ Help.test/Program.cs
@@ -0,0 +1,23 @@
+﻿using ozakboy.Help;
+
+namespace Help.test
+{
+    internal class Program
+    {
+        public class Bas
+        {
+            public string Ob { get; set; }
+            public string inoo { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var a = new Bas
+            {
+                inoo = "ad",
+                Ob = "oof"
+            };
+            var b = a.ToJsonString();
+        }
+    }
+}
